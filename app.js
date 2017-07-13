080a41299906d54321c38635327aee8585fbc89f
vgalenko <vitaliygalenko@gmail.com>
2017-07-13 16:48:14 -0700

    added event listeners and button feature

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const myHeading = document.getElementsByTagName('h1')[0];
+const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+const myTextInput = document.getElementById('titleColorInput');
+const myTextInput2 = document.getElementById('titleSizeInput');
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
 });
 
-const myHeading2 = document.getElementsByTagName('ul')[0];
-const myButton2 = document.getElementById('myButton');
-const myTextInput2 = document.getElementById('myTextInput2');
+myButton.addEventListener('click', () => {
+  myHeading.style.fontSize = myTextInput2.value;
+});
 
-myButton2.addEventListener('click', () => {
-  myHeading2.style.fontSize = myTextInput2.value;
+
+const h2Tag = document.querySelectorAll('h2');
+// const myButton = document.getElementById('myButton');
+const myColorInput = document.getElementById('colorInput');
+const mySizeInput = document.getElementById('sizeInput');
+const myFontInput = document.getElementById('fontInput');
+
+
+myButton.addEventListener('click', () => {
+  //myHeading.style.color = myTextInput.value;
+  for (var i=0; i<h2Tag.length; i++) {
+    h2Tag[i].style.color = myColorInput.value;
+    h2Tag[i].style.fontSize = mySizeInput.value;
+    h2Tag[i].style.fontFamily = myFontInput.value;
+  }
 });
